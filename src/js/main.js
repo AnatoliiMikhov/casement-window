95560bbfedeb619b4030c1a8749be963302f6d6c
AnatoliiMikhov <anatoli.mihov@gmail.com>
2020-06-12 11:06:11 +0300

    tabs realized and added animation to tabsContent

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,20 @@
 
 import "./slider";
 import modals from "./modules/modals";
+import tabs from "./modules/tabs";
 
 window.addEventListener("DOMContentLoaded", () => {
     modals();
+
+    tabs(".glazing_slider",
+        ".glazing_block",
+        ".glazing_content",
+        "active",
+        "fadeInDown");
+
+    tabs(".decoration_slider",
+        ".no_click",
+        ".decoration_content > div > div",
+        "after_click",
+        "fadeInRight");
 });
